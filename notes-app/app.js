aea0c5b943cefcabc6d556d53e5728fb0e1479c0
serrones <fabioserrones@gmail.com>
2021-01-17 13:34:15 -0300

    Importing functions from modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,35 +1,5 @@
-//
-// Challenge: Append a message  to notes.txt
-//
-// 1. Use appendFileSync to append to the file
-//
-// 2. Run the Script
-//
-// 3. Check your work by opening the file and viewing the appended text
+const getNotes = require('./notes.js')
 
-/*
-Common javascript module import
+const msg = getNotes()
 
-const fs = require('fs')
-
-fs.writeFileSync('notes.txt', 'NodeJS Course!\n')
-
-fs.appendFileSync('notes.txt', '-------------')
-
-fs.appendFileSync('notes.txt', '\n\n')
-
-fs.appendFileSync('notes.txt', 'First app builded with node')
-*/
-
-
-// es6 module import
-import { writeFileSync, appendFileSync } from 'fs'
-
-
-writeFileSync('notes.txt', 'NodeJS Course!\n')
-
-appendFileSync('notes.txt', '-------------')
-
-appendFileSync('notes.txt', '\n\n')
-
-appendFileSync('notes.txt', 'First app builded with node')
+console.log(msg)
---
+++ notes-app/notes.js
@@ -0,0 +1,18 @@
+/*
+Challenge
+
+Create a new file called notes.js
+
+Create getNotes function that returns 'Your Notes...'
+
+Export getNotes function
+
+From app.js, load and call the function printing message to console
+*/
+
+const getNotes = function() {
+
+    return 'Your Notes...'
+}
+
+module.exports = getNotes
